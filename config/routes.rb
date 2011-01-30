c5fbd307bd907a70dae41a7b6bce193dca35bfb8
Max Mustermann <maxmustermann@eml.cc>
2011-01-30 13:55:43 +0100

    finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
+  match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-  match '/contact', :to => 'pages#contact'
-
+  
   root :to => "pages#home"
 
   # The priority is based upon order of creation:
